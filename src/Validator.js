e229bdca5f8c2a1ebc8eeef8f8b9f11ae27488bf
rixiobarrios <rixiobarrios.consulado@gmail.com>
2020-01-14 19:21:02 -0600

    Commited, thanks!

--- src/Validator.js
+++ src/Validator.js
@@ -2,30 +2,62 @@ import React, { Component } from 'react';
 import './Validator.css';
 
 class Validator extends Component {
-  render() {
-    return (
-      <div className="form">
-        <h1>Sign Up</h1>
-        <form>
-          <input type="text" placeholder="Username" id="username" />
-          <label htmlFor="username">Username</label>
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            password: '',
+            passwordConfirm: '',
+            valid: true
+        };
+    }
+    handleChange = event => {
+        this.setState({ [event.target.id]: event.target.value });
+    };
+    handleSubmit = event => {
+        event.preventDefault();
+        if (this.state.password === this.state.passwordConfirm) {
+            alert('Password is a match');
+        } else {
+            alert('Password in not a match');
+        }
+    };
 
-          <input type="password" placeholder="Password" id="password" />
-          <label htmlFor="password">Password</label>
+    render() {
+        return (
+            <div className="form">
+                <h1>Sign Up</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <input
+                        onChange={this.handleChange}
+                        type="text"
+                        placeholder="Username"
+                        id="username"
+                    />
+                    <label htmlFor="username">Username</label>
 
-          <input
-            type="password"
-            placeholder="Confirm password"
-            id="passwordConfirm"
-          />
-          <label htmlFor="passwordConfirm">Confirm password</label>
+                    <input
+                        onChange={this.handleChange}
+                        type="password"
+                        placeholder="Password"
+                        id="password"
+                    />
+                    <label htmlFor="password">Password</label>
 
-          <button type="submit">Sign Up</button>
-          <p>Passwords must match.</p>
-        </form>
-      </div>
-    );
-  }
+                    <input
+                        onChange={this.handleChange}
+                        type="password"
+                        placeholder="Confirm password"
+                        id="passwordConfirm"
+                    />
+                    <label htmlFor="passwordConfirm">Confirm password</label>
+
+                    <button type="submit">Sign Up</button>
+                    <p>Passwords must match.</p>
+                </form>
+            </div>
+        );
+    }
 }
 
 export default Validator;
